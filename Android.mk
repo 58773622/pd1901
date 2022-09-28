7bb2ab537718bde9aed3b7290cdac1bf8cbeaf48
58773622 <59197362+58773622@users.noreply.github.com>
2022-09-28 13:43:27 +0800

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-# Android device tree for vivo vivo PD1901 (k65v1_64_bsp)
-
-```
 #
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),k65v1_64_bsp)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
--- device.mk
+++ device.mk
@@ -1,10 +1,8 @@
-# Android device tree for vivo vivo PD1901 (k65v1_64_bsp)
-
-```
 #
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := device/vivo/k65v1_64_bsp
